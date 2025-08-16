ba050f1866459c14d7a84b774d612a84fa157a1e
Fairfarren <lykwowlol@gmail.com>
2025-08-16 13:05:31 +0800

    chore: 注视

--- apps/example-vue/rsbuild.config.ts
+++ apps/example-vue/rsbuild.config.ts
@@ -17,4 +17,16 @@ export default defineConfig({
     }),
     pluginVueJsx(),
   ],
+  // 如果使用vite，请使用下面这个写法
+  // import vueJsx from '@vitejs/plugin-vue-jsx'
+  // plugins: [
+  //   vue({
+  //     template: {
+  //       compilerOptions: {
+  //         isCustomElement: (tag) => tag.includes('-'),
+  //       },
+  //     },
+  //   }),
+  //   vueJsx(),
+  // ],
 })
